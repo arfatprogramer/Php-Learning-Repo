2794972ef24b94017681edcac97e961f4ae46e2c
aratprogramer <arfata701@gmail.com>
2024-04-10 08:57:40 +0530

    First php page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Tut1</title>
+</head>
+
+<body>
+    <h1>Hello </h1>
+    <?php 
+    // for single line comment
+    /* Multi
+     line
+      comment
+    */
+    # single line comment
+
+    echo "<h1> Hi  Mo Arfat Ansari</h1>";
+
+    ?>
+</body>
+
+</html>
